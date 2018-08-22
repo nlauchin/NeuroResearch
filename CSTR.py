cae286c32c3baed1c1a6ec11819f15860e5d3619
nlauchin <nlauchin@gmail.com>
2018-08-22 18:14:22 -0400

    Update CSTR.py

--- CSTR.py
+++ CSTR.py
@@ -1,12 +1,11 @@
 from netpyne import specs,sim
 
 netParams = specs.NetParams()
-simConfig = specs.simConfig()
-netParams.popParams['CSTR_pop'] = {'cellType': 'CSTR','numCells':2, 'cellModel': 'HH'}
+netParams.popParams['CSTR_pop'] = {'cellType': 'CSTR','numCells':1, 'cellModel': 'HH'}
 
-cellRule = {'conds': {'cellType':'CSTR', 'cellModel':'HH'}, 'secs': {}}
+cellRule = {'conds': {'popLabel': 'CSTR_pop'}, 'secs': {}}
 soma = {'geom':{}, 'mechs':{}}
-soma['geom'] = {'diam': 44.3, 'L': 23.9, 'Ra':123.0, 'pt3d': []}
+soma['geom'] = {'diam': 44.3, 'L': 23.9, 'Ra':123.0}
 soma['mechs']['hh'] = {'gnabar': 0.13, 'gkbar': 0.036, 'gl': 0.003, 'el': -65}
 
 dend = {'geom': {}, 'topol': {}, 'mechs': {}}
@@ -15,18 +14,21 @@ dend['topol'] = {'parentSec': 'soma', 'parentX': 1.0, 'childX': 0}
 dend['mechs']['pas'] = {'g': 0.0000357, 'e': -70}
 
 cellRule['secs'] = {'soma':soma, 'dend': dend}
-netParams.cellParams['CSTR_HH'] = cellRule
+netParams.cellParams['CSTRrule'] = cellRule
 
 netParams.synMechParams['AMPA'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 1.0, 'e': 0}
 
 netParams.addConnParams('CSTR_pop->CSTR_pop',{'preConds': {'popLabel': 'CSTR_pop'}, 'postConds': {'postLabel': 'CSTR_pop'}, 'delay':5, 'sec':'dend', 'loc':1.0, 'synMech':'exc'})
+
 netParams.stimSourceParams['Input_1'] = {'type': 'NetStim', 'rate': 10, 'noise': 0.5, 'start': 1}
-netParams.stimTargetParams['Input_1->CSTR'] = {'source': 'Input_1', 'sec':'soma', 'loc':0.5, 'conds': {'popLabel': 'CSTR_pop'}}
+
+netParams.stimTargetParams['Input_1->CSTR_pop'] = {'source': 'Input_1', 'sec':'soma', 'loc':0.5, 'conds': {'popLabel': 'CSTR_pop'}}
 
 simConfig = specs.SimConfig()
 simConfig.duration = 70
 simConfig.dt = 0.025
 simConfig.verbose = False
+simConfig.recordTraces = {'V_Dend': {'sec':'dend', 'loc':0.5, 'var':'v'}}
 simConfig.recordTraces = {'V_Soma': {'sec':'soma', 'loc':0.5, 'var':'v'}}
 simConfig.seeds = {'conn':1, 'stim':1, 'loc':1}
 simConfig.createNEURONObj = 1
@@ -34,10 +36,7 @@ simConfig.recordStim = True
 simConfig.recordStep = 0.5
 simConfig.filename = 'CSTR_output'
 simConfig.savePickle = True
-simConfig.analysis['plotRaster'] = True
+simConfig.analysis['plotRaster'] = {'saveFig': True}
 simConfig.analysis['plotTraces'] = {'include': [0]}
 simConfig.analysis['plot2Dnet'] = True
-
-sim.createSimulateAnalyze(netParams, simConfig)
-
 
