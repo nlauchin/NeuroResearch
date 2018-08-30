10e362e221fb68a3fb7c7bca05219cb46703b020
nlauchin <nlauchin@gmail.com>
2018-08-30 17:33:53 -0400

    Update CSTR.py

--- CSTR.py
+++ CSTR.py
@@ -22,7 +22,7 @@ cellRule['secLists']['spiny'] = [sec for sec in cellRule['secLists']['alldend'] 
 
 
 
-netParams.popParams['CSTR_pop'] = {'cellType': 'CSTR','numCells':1, 'cellModel': 'HH'}
+netParams.popParams['IT_full_BS1578'] = {'cellType': 'IT','numCells':1, 'cellModel': 'HH'}
 
 #cellRule = {'conds': {'popLabel': 'CSTR_pop'}, 'secs': {}}
 #soma = {'geom':{}, 'mechs':{}}
@@ -40,11 +40,15 @@ netParams.popParams['CSTR_pop'] = {'cellType': 'CSTR','numCells':1, 'cellModel':
 netParams.synMechParams['AMPA'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 1.0, 'e': 0}
 netParams.synMechParams['NMDA'] = {'mod': 'MyExp2SynNMDABB', 'tau1NMDA':15,'tau2NMDA': 150, 'e': 0}
 
-netParams.stimSourceParams['Input_1'] = {'type': 'NetStim', 'rate': 10, 'noise': 0.5, 'start': 1}
+#netParams.addConnParams('IT_full_BS1578->IT_full_BS1578',{'preConds': {'popLabel': 'IT_full_BS1578'}, 'postConds': {'postLabel': 'IT_full_BS1578'}, 'delay':5, 'sec':'dend', 'loc':1.0, 'synMech':'exc'})
+
+netParams.stimSourceParams['Input_1'] = {'type': 'NetStim', 'rate': 20, 'noise': 0.5, 'start': 1}
 netParams.stimSourceParams['Input_2'] = {'type': 'IClamp', 'delay': 3, 'dur': 10, 'amp': 30}
 
-netParams.stimTargetParams['Input_1->CSTR_pop'] = {'source': 'Input_1', 'sec':'soma', 'loc':0.5, 'conds': {'popLabel': 'CSTR_pop'}}
-netParams.stimTargetParams['Input_2->CSTR_pop'] = {'source': 'Input_2', 'sec':'dend', 'loc':1.0, 'delay': 0, 'conds':{'popLabel': 'CSTR_pop'}}
+netParams.stimTargetParams['Input_1->IT_full_BS1578'] = {'source': 'Input_1', 'sec':'soma', 'loc':0.5, 'weight': 0.01, 'synMech': 'exc', 'conds': {'popLabel': 'IT_full_BS1578'}}
+netParams.stimTargetParams['Input_2->IT_full_BS1578'] = {'source': 'Input_2', 'sec':'dend', 'loc':1.0, 'delay': 0, 'synMech': 'exc', 'conds':{'popLabel': 'IT_full_BS1578'}}
+
+print netParams.popParams
 
 simConfig = specs.SimConfig()
 simConfig.duration = 70
@@ -52,7 +56,7 @@ simConfig.dt = 0.025
 simConfig.verbose = False
 simConfig.recordTraces = {'V_Dend': {'sec':'dend', 'loc':0.5, 'var':'v'}}
 simConfig.recordTraces = {'V_soma': {'sec':'soma', 'loc':0.5, 'var':'v'}}
-simConfig.seeds = {'conn':1, 'stim':1, 'loc':1}
+simConfig.seeds = {'conn':1, 'stim':2, 'loc':1}
 simConfig.createNEURONObj = 1
 simConfig.recordStim = True
 simConfig.recordStep = 0.5
