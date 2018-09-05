02c9ae069d40823dfc831d9c38c9a54c3ae89b29
nlauchin <nlauchin@gmail.com>
2018-09-05 16:56:45 -0400

    Update CSTR_init.py

--- CSTR_init.py
+++ CSTR_init.py
@@ -5,17 +5,5 @@ simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='CSTR_cfg.py', netPa
 sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
 
 
-sim.initialize(
-    simConfig=cfg, 
-    netParams=netParams)  # create network object and set cfg and net params
-sim.net.createPops()                  # instantiate network populations
-sim.net.createCells()                 # instantiate network cells based on defined populations
-sim.net.connectCells()                # create connections between cells based on params
-sim.setupRecording()              # setup variables to record for each cell (spikes, V traces, etc)
-sim.runSim()                      # run parallel Neuron simulation  
-sim.gatherData()                  # gather spiking data and cell info from each node
-sim.saveData()                    # save params, cell info and sim output to file (pickle,mat,txt,etc)
-sim.analysis.plotData()               # plot spike raster
 
 import pylab; pylab.show()
-                                                                                                                                                                                                                                                                                                                                                                    
